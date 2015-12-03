248e413c434f98383ce0a5dda82cab832074b641
lechiffre10 <sabri.helal@gmail.com>
2015-12-02 17:35:56 -0700

    Add Flow control challenges solutions for week 4

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,14 @@
+# Simple Substrings
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+def welcome(address)
+	if address.include?("CA")
+		return "Welcome to California"
+	else
+		return "You should move to California"
+	end
+end
+#https://github.com/lechiffre10/phase-0/blob/master/week-4/simple-substrings/my_solution.rb
