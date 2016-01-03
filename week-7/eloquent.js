cfa899c8ec2f95e023671c8865925f07b085c73e
lechiffre10 <sabri.helal@gmail.com>
2016-01-02 19:29:53 -0500

    Add solution to eloquent.js

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -4,19 +4,36 @@
 
 // Program Structure
 // Write your own variable and do something to it.
-
-
+var number= 5;
+console.log(number * number);
 
 
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+//Looping a triangle
+for (var string="#";string.length <= 7; string += "#")
+console.log(string);
 
 
 
 // Functions
+prompt("What is your favorite food?");
+alert("Hey! that is my favorite food too!");
 
 // Complete the `minimum` exercise.
-
+function min(a,b) {
+	if (a < b)
+		return a;
+	else
+		return b;
+};
 
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+var me = {
+	name: "Sabri Helal",
+	age: 28,
+	favoriteFood: ["pizza","lasagna","ramen"],
+	quirk: "Can sing like there's no tomorrow"
+};
+console.log(me)
